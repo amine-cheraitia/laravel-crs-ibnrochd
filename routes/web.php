97477ad4e9910fe5d4402e93ba107f341c5ac1d6
amine-cheraitia <amine-cheraitia@hotmail.com>
2021-09-10 18:43:18 +0200

    ide helper

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,46 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/ibnrochd', function () {
+    return "erreur!!";
+})->name('ibnrochd');
+
+//avec paramétre obligatoire
+Route::get('/commande/{x}', function ($x) {
+    return "la commande recherché est : $x";
+});
+
+// avec paramétre optionel
+/*Route::get('/client/{x?}', function ($x = 1) {
+    if ($x > 3) return "erreur";
+    return "le client recherché est le numéro : $x";
+});
+
+
+*/
+Route::get('/client/{x?}', function ($x = 1) {
+    if ($x > 3) return redirect()->route('ibnrochd');
+    return "le client recherché est le numéro $x";
+});
+
+
+
+// avec deux paramétre
+
+Route::get('/article/{x}-{y}', function ($x, $y) {
+    return "l'article recherché est: $x-$y";
+});
+
+//exemple avec controlleur
+route::get('/fournisseurs', 'FournisseurController@index')->name('fournisseurs');
+
+/*
+Route::get('/ibnrochd{$x}', function ($x) {
+    return "erreur numéro $x!!";
+})->name('ibnrochd');
+*/
+Route::get('/client/{x?}', function ($x = 1) {
+    if ($x > 3) return redirect()->route('ibnrochd', ['x' => 20]);
+    return "le semestre recherché est le numéro $x";
+});
